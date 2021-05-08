d9cbd2815c332a4a4fe3b35ac6130f06a3fe2574
nitikornchumnankul <nitikornchumnankul@gmail.com>
2021-05-09 06:20:32 +0700

    เพิ่ม link การใช้ numpy.array

--- api/generateClassifier.py
+++ api/generateClassifier.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import fetch_openml
+import numpy as np
 
 # โหลด Dataset
 # Fixes #11317
@@ -19,6 +20,14 @@ from sklearn.datasets import fetch_openml
 # x = mnist.data
 # y = mnist.target
 # https://stackoverflow.com/questions/47324921/cant-load-mnist-original-dataset-using-sklearn
+# https://www.programcreek.com/python/example/117644/sklearn.datasets.fetch_openml
+
 dataset = fetch_openml('mnist_784')
 
 # Extract the features and labels
+#Data Type and How to use numpy.array
+#https://numpy.org/doc/stable/reference/generated/numpy.array.html?highlight=array#numpy.array
+feature = np.array(dataset.data, 'int16')
+label = np.arry(dataset.target, 'int')
+
+
