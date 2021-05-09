2f85c3045ff71e11c44b3797e3683c4952a5d366
nitikornchumnankul <nitikornchumnankul@gmail.com>
2021-05-09 10:39:28 +0700

    เพิ่มรูปในการทดสอย

--- api/performRecognition.py
+++ api/performRecognition.py
@@ -142,7 +142,7 @@ for rect in rects:
     roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
     roi = cv2.dilate(roi, (3, 3))
     # Calculate the HOG features
-    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualize=False)
     roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
     nbr = clf.predict(roi_hog_fd)
     cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
